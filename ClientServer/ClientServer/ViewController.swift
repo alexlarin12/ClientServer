507eb523bd02d7d5d9d9813f6b1dfe7ca806aacd
alexlarin <alex12081973@mail.ru>
2019-10-21 17:15:38 +0300

    created new project

---
+++ ClientServer/ClientServer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClientServer
+//
+//  Created by Alex Larin on 21/10/2019.
+//  Copyright © 2019 Alex Larin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ClientServer/ClientServerTests/ClientServerTests.swift
@@ -0,0 +1,34 @@
+//
+//  ClientServerTests.swift
+//  ClientServerTests
+//
+//  Created by Alex Larin on 21/10/2019.
+//  Copyright © 2019 Alex Larin. All rights reserved.
+//
+
+import XCTest
+@testable import ClientServer
+
+class ClientServerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
